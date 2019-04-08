6f4cbe717d4b9de7d553d5b83f5052f3faaf1be3
Danijel Ilisin <danijel.ilisin@ext.parshipelite.com>
2019-04-08 14:37:39 +0200

    reordering admin user

--- manifests/replset.pp
+++ manifests/replset.pp
@@ -8,6 +8,7 @@ class mongodb::replset(
   }
 
   # Order replset before any DB's and shard config
+  Mongodb_adminuser <| |> -> Mongodb_replset <| |>
   Mongodb_replset <| |> -> Mongodb_database <| |>
   Mongodb_replset <| |> -> Mongodb_shard <| |>
   Mongodb_replset <| |> -> Mongodb_user <| |>
